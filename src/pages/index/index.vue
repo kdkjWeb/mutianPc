babf3b40f5fc4f54e364f20e5c33aee8e418010c
ong-king <1318281415@qq.com>
2018-05-17 16:22:12 +0800

    添加index 文件

---
+++ src/pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="index">
+		
+		<header>
+			
+		</header>
+		
+		<nav>
+			<ul>
+				<li>用户管理</li>
+				<li>会员业绩表</li>
+				<li>商品管理</li>
+				<li>商品评论</li>
+				<li>商城资讯</li>
+				<li>系统设置</li>
+			</ul>
+			
+		</nav>
+	</div>
+</template>
+
+<script>
+	export {default} from './indexCtr'
+</script>
+
+<style scoped>
+	.index{
+		height: 100%;
+	}
+	header{
+		position: fixed;
+		top: 0;
+		background-color: #4768f3;
+		height: 70px;
+		width: 100%;
+	}
+	nav{
+		width: 160px;
+		height: 100%;
+		background-color: #022e55;
+	}
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,14 @@
 //整个项目的路由文件
 
-
+const index = () => import('@/pages/index/index');  //首页
 
 
 export default{
     routes: [
-        
+        {
+        	path: '/',
+        	name: 'index',
+        	component: index
+        }
     ]
 }
