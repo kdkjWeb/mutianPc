9c4f7226a175907212cbeea51f3a70e32ba5e746
Mac <1270201010@qq.com>
2018-05-17 16:19:19 +0800

    简单修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,37 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+html,body{
+  width: 100%;
+  height: 100%;
+}
+#app{
+  width: 100%;
+  height: 100%;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+}
+ul,ol{
+  list-style: none;
+}
+img{
+  border: none;
+}
+a{
+  text-decoration: none;
+}
+input{
+  outline: none;
 }
 </style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,14 @@
 //整个项目的路由文件
-
+const login = ()=>import('@/pages/login/login')  //登录
 
 
 
 export default{
     routes: [
-        
+        {
+            path: '/',
+            name: 'login',
+            component: login
+        }
     ]
 }
