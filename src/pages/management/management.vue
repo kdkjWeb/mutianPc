89f0713cd0230b55ce86ecf0485414b35ed97e21
ong-king <1318281415@qq.com>
2018-05-18 11:01:48 +0800

    用户管理表格

--- src/pages/management/management.vue
+++ src/pages/management/management.vue
@@ -1,48 +1,106 @@
 <template>
-	<div class="management"><!--用户管理-->
-		
+	<div class="management">
+		<!--用户管理-->
+
 		<div class="header">
-			<input type="text" name="" id="" value="" placeholder="可按电话号码，昵称 查找"/>
-			
+			<input type="text" name="" id="" value="" placeholder="可按电话号码，昵称 查找" />
+
 			<div class="search">
 				<i class="el-icon-search"></i>
 				<span>查询</span>
 			</div>
+
+			<input type="button" value="修改用户等级" class="changeNum" />
+			<input type="button" value="修改密码" />
+			<input type="button" value="封号" />
+			<input type="button" value="解封" />
+			<input type="button" value="删除" />
+
+		</div>
+
+	
+		<div class="content">
 			
-			<input type="button" value="修改用户等级"/>
-			<input type="button" value="修改密码"/>
-			<input type="button" value="封号"/>
-			<input type="button" value="解封"/>
-			<input type="button" value="删除"/>
+			<!--==表格====-->
+			<div class="Table">
+				
+				<el-table ref="multipleTable"  :height="h" :data="tableData3" tooltip-effect="dark" border style="width: 100%" @selection-change="handleSelectionChange">
+					<el-table-column type="selection" width="55"></el-table-column>
+	
+					<el-table-column prop="Phone" label="手机号码" width="120"></el-table-column>
+	
+					<el-table-column prop="name" label="真实姓名" width="80"></el-table-column>
+	
+					<el-table-column prop="nickname" label="昵称" width="100"></el-table-column>
+	
+					<el-table-column prop="upId" label="上级会员ID" width="100"></el-table-column>
+	
+					<el-table-column prop="upName" label="上级会员姓名" width="110"></el-table-column>
+	
+					<el-table-column prop="level" label="所属层级" width="80"></el-table-column>
+	
+					<el-table-column prop="grade" label="用户等级" width="80"></el-table-column>
+	
+					<el-table-column prop="card" label="银行卡号" width="150"></el-table-column>
+	
+					<el-table-column prop="bank" label="开户银行" width="80"></el-table-column>
+	
+					<el-table-column prop="vip" label="是否会员" width="80"></el-table-column>
+	
+					<el-table-column prop="money" label="累计消费金额" width="110"></el-table-column>
+	
+					<el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column>
+	
+				</el-table>
+	
+				<div class="foot">
+				<div class="footLeft">
+					注册用户数：5555
+				</div>
+				<div class="footNum">
+					<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="currentPage3" :page-size="100" layout="prev, pager, next, jumper" :total="1000"></el-pagination>
+				</div>
+			</div>
+
+			</div>
+			
+			<div class="Copyright">Copyright ©2018-2019     成都恺缔科技有限公司</div>
 			
 		</div>
-		
-		
+
 	</div>
 </template>
 
 <script>
-	export {default} from './managementCtr'
+	export {
+		default
+	}
+	from './managementCtr'
 </script>
 
 <style scoped>
-	.management{
+	.management {
 		width: 100%;
 		height: 100%;
 	}
-	.header{
+	
+	.header {
 		height: 80px;
 		line-height: 80px;
 		background-color: #FFFFFF;
 	}
-	.header input[type="text"]{
+	
+	.header input[type="text"] {
+		/*/搜索输入框*/
 		width: 270px;
 		height: 35px;
 		border-radius: 20px;
 		text-indent: 15px;
 		border: 1px solid #b9b9b9;
+		margin-left: 30px;
 	}
-	.search{
+	
+	.search {
 		display: inline-block;
 		height: 35px;
 		line-height: 35px;
@@ -53,11 +111,14 @@
 		text-align: center;
 		font-size: 18px;
 		cursor: pointer;
+		margin-left: 10px;
 	}
-	.search i{
+	
+	.search i {
 		margin-right: 10px;
 	}
-	.header input[type="button"]{
+	
+	.header input[type="button"] {
 		height: 35px;
 		border-radius: 20px;
 		background-color: #0085e2;
@@ -66,5 +127,54 @@
 		color: #FFFFFF;
 		padding: 0 20px;
 		cursor: pointer;
+		margin-left: 30px;
 	}
+	
+	.changeNum {
+		margin-left: 10%!important;
+	}
+	/*============表格样式覆盖===========*/
+	
+	.content {
+		padding: 10px;
+		background-color: #FFFFFF;
+		height: calc(100% - 100px);
+		/*overflow: hidden;*/
+	}
+	.Table{
+		height: calc(100% - 30px);
+		border: 1px solid #c2c2c2;
+		border-radius: 15px;
+/*		overflow: hidden;*/
+		position: relative;
+	}
+	.el-table--fit {
+		height: calc(100% - 70px);
+	}
+	
+	/*=====底部======*/
+	.foot{
+		padding: 30px 0;
+		text-align: center;
+		position: absolute;
+		bottom: 0;
+	}
+	.foot div{
+		display: inline-block;
+		color: #7e7e7e;
+	}
+	.footNum{
+		margin: 0 auto;
+	}
+	.footLeft{
+		float: left;
+		margin-left: 80px;
+	}
+	.Copyright{
+		color: #a1a1a1;
+		font-size: 16px;
+		text-align: center;
+		line-height: 30px;
+	}
+	
 </style>
--- src/pages/management/managementCtr.js
+++ src/pages/management/managementCtr.js
@@ -1,7 +1,187 @@
-export default {
-	data() {
-		return {
-			input: ''
-		}
-	}
-}
+  export default {
+    data() {
+      return {
+      	input:'',//搜索框 的值
+      	currentPage3: 5,   //分页的页数
+      	h:350,
+        tableData3: [{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        },{
+        	Phone:'1842656566',
+        	name: '王小虎',
+        	nickname:'昵称昵称',
+       		upId:'15',
+       		upName:'加加',
+       		level:'用户',
+       		grade:'三级',
+       		card:'5546513211323',
+       		bank:'广大银行',
+       		vip:'否',
+       		money:'50005',
+       		address:'成都市武侯区达到路口第二家扬州炒饭'
+        }],
+        multipleSelection: []
+      }
+    },
+
+    methods: {
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },  
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      }
+
+    },
+    
+    created(){
+    	let s = document.body.clientHeight;
+    	this.h = s - 300;
+    	console.log(this.h)
+    },
+    mounted(){
+    	
+    }
+    
+    
+  }
